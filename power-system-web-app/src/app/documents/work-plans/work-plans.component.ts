081c906bb71e3049fb7118b223dcfad8ae76d973
Predrag <svetoizvorac@yahoo.com>
2021-03-26 20:03:19 +0100

    add global container styles to styles.css

--- power-system-web-app/src/app/documents/work-plans/work-plans.component.ts
+++ power-system-web-app/src/app/documents/work-plans/work-plans.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-work-plans',
   templateUrl: './work-plans.component.html',
-  styleUrls: ['./work-plans.component.css']
+  styleUrls: ['./work-plans.component.css'] 
 })
 export class WorkPlansComponent implements OnInit {
 
