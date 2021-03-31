6f1a0b26e99f1b19a7214bd9fda82100a44dfeb7
Predrag <svetoizvorac@yahoo.com>
2021-03-31 21:42:53 +0200

    Added incidents resolution page

--- power-system-web-app/src/app/incidents/resolution/resolution.component.ts
+++ power-system-web-app/src/app/incidents/resolution/resolution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-resolution',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resolution.component.css']
 })
 export class ResolutionComponent implements OnInit {
+  failureSubcauses:string[] = ['failuresub1', 'failuresub2', 'failuresub3'];
+  weatherSubcauses:string[] = ['Storm', 'Rain', 'Wind', 'Snow'];
+  humanErrorSubcauses:string[] = ['1', '2', '3', '4'];
+  subcauses:string[] = [];
+  resolutionFormControl= new FormControl();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  causeSelectionChanged(event:any)
+  {
+    if(event.value === 'Failure')
+    {
+      this.subcauses = this.failureSubcauses;
+
+    }else if(event.value === 'Weather')
+    {
+      this.subcauses = this.weatherSubcauses;
+
+    }else if(event.value === 'HumanError')
+    {
+      this.subcauses = this.humanErrorSubcauses;
+    }
+  }
+
+  get shouldShowSubcauses():boolean{
+    return this.subcauses.length > 0;
   }
 
 }
