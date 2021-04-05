77eef09d45611a66811b7d4fa9700e4741f4aa4e
Nikola Mijonic <nikolamijonic@gmail.com>
2021-04-05 23:37:06 +0200

    Added safety-documents and components related to safety-document

---
+++ power-system-web-app/src/app/documents/safety-documents/safety-document-checklist/safety-document-checklist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-safety-document-checklist',
+  templateUrl: './safety-document-checklist.component.html',
+  styleUrls: ['./safety-document-checklist.component.css']
+})
+export class SafetyDocumentChecklistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ power-system-web-app/src/app/documents/safety-documents/safety-document-state-changes/safety-document-state-changes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-safety-document-state-changes',
+  templateUrl: './safety-document-state-changes.component.html',
+  styleUrls: ['./safety-document-state-changes.component.css']
+})
+export class SafetyDocumentStateChangesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
